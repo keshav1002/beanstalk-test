3e774f81bc627fc17b9e7b7a6e3fd07b77c921c5
Keshav Kumaresan <keshavkumaresan1002@gmail.com>
2021-07-30 18:50:31 +0530

    Modified port number

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.get('/', (req, res)=>{
     res.send({message:"Welcome to KK's Home Page!!!"});
 });
 
-app.listen(3000, ()=>{
+app.listen(process.env.PORT || 3000, ()=>{
     console.log("Hello World, Its me KK!!");
 });
