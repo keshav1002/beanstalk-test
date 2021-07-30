f91ffc1f0054e5bf0a9089a2386fdae3906b2b31
Keshav Kumaresan <keshavkumaresan1002@gmail.com>
2021-07-30 19:00:35 +0530

    added comments

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ app.get('/', (req, res)=>{
     res.send({message:"Welcome to KK's Home Page v2!!!"});
 });
 
+// the port variable taken from the environment so that it will use the port configured from AWS
 app.listen(process.env.PORT || 3000, ()=>{
     console.log("Hello World, Its me KK!!");
 });
